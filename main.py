66001cb47ad2fdf7aef93627bc265bfc92b4c7b5
Ritis Barauskas <perm300ru@yandex.ru>
2024-07-04 19:32:49 +0300

    added Human class

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-print('Hello, world')
-for i in range(10):
-    print(i)
+
+
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+def main():
+    john = Human("John", 30)
+    bill = Human("Bill", 25)
+    maxim = Human("Max", 35)
+
+    students = [john, bill, maxim]
+
+    for student in students:
+        student.say_hello()
+
+
+if __name__ == "__main__":
+    main()
